545d35e75d0533ddc09e432f1bae468f7fefc17a
sandra-sedano <113152271+Sandra-Sedano@users.noreply.github.com>
2023-05-08 22:04:07 -0400

    layoutTarefasCadastradas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { Theme } from "./theme/Theme"
-import { AppBar, Badge, Button, colors, Grid, TextField, Toolbar, Typography, useTheme } from "@mui/material";
-import { Container } from "@mui/system";
-import { PlusCircle, Rocket } from "phosphor-react";
+import { AppBar, Badge, Button, colors, Divider, Grid, TextField, Toolbar, Typography, useTheme } from "@mui/material";
+import { Container, spacing } from "@mui/system";
+import { PlusCircle, Rocket, ClipboardText } from "phosphor-react";
+import { grey } from '@mui/material/colors';
+import { useState } from 'react';
 
 function App() {
   const tema = useTheme()
@@ -50,17 +52,47 @@ function App() {
             <Grid container spacing={tema.spacing(1)}>
               <Grid item>
                 <Typography variant="caption" sx={{
-                  display:'flex',
-                  alignItems:'center',
-                  gap:tema.spacing(1)
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: tema.spacing(1)
 
                 }}>
-                 <Badge color='primary' badgeContent={0} showZero> Tarefas Criadas</Badge>  
-
+                  <Badge color='primary' badgeContent={0} showZero> Tarefas Criadas</Badge>
+                  <Badge  color='primary' badgeContent={0} showZero> Concluidas</Badge>
                 </Typography>
 
               </Grid>
+
             </Grid>
+
+            <Divider />
+
+            <Grid  container sx={{
+              textAlign: 'center',
+              alignItems: 'center',
+             padding: '50px',
+             
+            }}>
+              <Grid xl={12} xs={12}>
+                <ClipboardText size={50} />
+                <Typography sx={{
+                  color: colors.grey[100],
+                  fontSize: '20px'
+                
+                }}>
+                  Você ainda não tem tarefas cadastradas
+                </Typography>
+
+                <Typography sx={{
+                  color: colors.grey[500],
+                  fontSize: '18px'
+                }}>
+                  Crie tarefas e organize seus itens a fazer
+                </Typography>
+              </Grid>
+
+            </Grid>
+
           </Container>
         </main>
       </Theme>
