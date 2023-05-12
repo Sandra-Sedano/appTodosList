353404bfb44107c7992cfb725e59e5cd8d2900a9
sandra-sedano <113152271+Sandra-Sedano@users.noreply.github.com>
2023-05-11 20:59:21 -0400

    atualizacao do Task

---
+++ src/service/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import { Task } from '../types';
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000',
+    timeout: 3000
+});
+//Listar todos os registros 
+export async function getAll() {
+    const tasks= await api.get('tasks')
+    return tasks.data
+}
+//buscar registro 
+export async function getById(id:number) {
+    const tasks= await api.get(`tasks/${id}`)
+    return tasks.data
+}
+// salvar registro
+export async function save(tasks:Task) {
+    const response= await api.post(`tasks`,{
+        ...tasks
+    })
+    return response.data
+}
+// atualizar registro 
+export async function update(tasks:Task) {
+    const response= await api.put(`tasks/${id}`, {
+        ...tasks
+    })
+    return response.data
+}
+//excluir registro 
+export async function exclude(id:number) {
+    const tasks= await api.delete(`tasks`)
+    return tasks.data
+}
